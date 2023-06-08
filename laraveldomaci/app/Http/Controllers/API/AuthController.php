0f71b78b3e1cb137c4b8ee3579b64fe934caa28f
marijaoo <ocelmarija@gmail.com>
2023-06-09 00:44:16 +0200

    Dodavanje Laravel Sanctum autentifikacije

---
+++ laraveldomaci/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Laravel\Sanctum\HasApiTokens;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function registracija(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8'
+
+        ]);
+
+        if ($validator->fails()) {
+
+            return response()->json($validator->errors());
+
+        }
+
+        $user = User::create([
+
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+
+            ->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer']);
+
+    }
+
+    public function prijava(Request $request)
+    {
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+
+            return response()
+                ->json(['message' => 'Nemas autorizovani pristup.'], 401);
+
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json(['message' => 'Dobrodosli ' . $user->name . '.', 'access_token' => $token, 'token_type' => 'Bearer']);
+
+    }
+
+    public function odjava(Request $request)
+    {
+
+        $request->user()->currentAccessToken()->delete();
+        return [
+
+            'message' => 'Uspesna odjava.'
+
+        ];
+
+    }
+}
--- laraveldomaci/routes/api.php
+++ laraveldomaci/routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\PozajmicaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,19 +18,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Auth
+
+Route::post('/registracija', [AuthController::class, 'registracija']);
+Route::post('/prijava', [AuthController::class, 'prijava']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/odjava', [AuthController::class, 'odjava']);
+
+    Route::resource('clans', ClanController::class);
+    Route::get('/clans', [ClanController::class, 'vratiSveClanove']);
+    Route::post('/clans', [ClanController::class, 'dodajClana']);
+    Route::delete('/clans/{clan}', [ClanController::class, 'obrisiClana']);
+
+
+    Route::resource('pozajmicas', PozajmicaController::class);
+    Route::get('/pozajmicas', [PozajmicaController::class, 'vratiSvePozajmice']);
+    Route::post('/pozajmicas', [PozajmicaController::class, 'dodajPozajmicu']);
+    Route::delete('/pozajmicas/{pozajmica}', [PozajmicaController::class, 'obrisiPozajmicu']);
+
+
 });
-
-Route::resource('clans', ClanController::class);
-
-Route::get('/clans', [ClanController::class, 'vratiSveClanove']);
-Route::post('/clans', [ClanController::class, 'dodajClana']);
-Route::delete('/clans/{clan}', [ClanController::class, 'obrisiClana']);
-
-
-Route::resource('pozajmicas', PozajmicaController::class);
-
-Route::get('/pozajmicas', [PozajmicaController::class, 'vratiSvePozajmice']);
-Route::post('/pozajmicas', [PozajmicaController::class, 'dodajPozajmicu']);
-Route::delete('/pozajmicas/{pozajmica}', [PozajmicaController::class, 'obrisiPozajmicu']);
